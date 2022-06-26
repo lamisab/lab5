8e38532198f8e1a0bd949b32bb0bcf15b2a50165
Lamis Abdullah <102637669+lamisab@users.noreply.github.com>
2022-06-26 14:17:17 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import { server } from './server';
 
+const port: any = process.env.PORT ?? process.env.$PORT ?? 3002;
 
-
-server.get('/',async(request, reply) => {
-    return { hello: 'hello' };
-  });
-  
-
-
-server.listen({ port: 3000 }).catch((err) => {
-	server.log.error(err);
-	process.exit(1);
-});
-
-
-
+server
+	.listen({
+		port: port,
+		host: '0.0.0.0',
+	})
+	.catch((err) => {
+		server.log.error(err);
+		process.exit(1);
+	});
